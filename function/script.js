acc5fa4e5a20d9abb139484416137c46f1656d0d
abdul1aev <dilshodbek.abdullaev.007@gmail.com>
2023-12-25 16:20:47 +0500

    function

---
+++ function/script.js
@@ -0,0 +1,71 @@
+"use strict"
+
+
+// function sayHello(a, b) {
+//     console.log('Hello world');
+//     console.log(a + b);
+// }
+
+// sayHello(5, 9);
+// // sayHello(5, 9);
+// // sayHello(5, 9);
+// // sayHello(5, 9);
+
+
+
+// let age = 20
+// function str() {
+//     age = 25;
+//     console.log(age);
+// }
+
+// str();
+// console.log(age);
+
+
+// function sum(a, b) {
+//     return a + b
+// }
+
+// console.log(sum (5, 6));
+
+
+// decloration
+
+
+// nom ();
+// function nom() {
+    // console.log('Dilshod');
+// }
+
+// nom ();
+
+
+// expression
+
+// const nom = function () {
+//     console.log('Dilshod');
+// }
+
+// nom();
+
+
+// arrow
+
+// const nom = (a, b) => (a + b);
+// nom(4 , 5);
+// console.log(nom(4, 5));
+
+
+
+
+// let str = 'dilshod';
+// let arr = [1, 2, 3, 4];
+// console.log(str.length);
+// console.log(arr.length);
+// let str2 = str.toUpperCase()
+    // console.log(str);
+    // console.log(str2);
+// console.log(str.lastIndexOf('d'), str.indexOf('d'));
+// console.log(str.slice(3, 7));
+// console.log(str.substring(3, 7));
